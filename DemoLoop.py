4fd606679275c0ebece6ff1097c52bc58b248083
seungukk <suk638663@gamil.com>
2022-06-21 11:23:49 +0900

    반복구문 연습2

--- DemoLoop.py
+++ DemoLoop.py
@@ -10,3 +10,40 @@ print("---반복 종료---")
 #순회가능한 형식(시퀀스형식)
 for item in [1,2,3]:
     print(item)
+
+
+#구구단 출력
+for x in [2,3,4,5,6]:
+    print("---{0}단 출력---".format(x)) #{0}포지션을 잡고 x값을 출력해
+    for y in [1,2,3,4,5,6,7,8,9]:
+        print("{0}*{1}={2}".format(x,y,x*y))  #0번자리 1번자리가 2번자리로 넘어감 
+
+
+print("---break---")
+lst = list(range(1,11))
+print(lst)
+for i in lst:
+    if i > 5:
+        break
+    print("item:{0}".format(i))
+
+print("---continue---")
+for i in lst:
+    #%는 나머지값 으로 짝수는 contunue 스킵해 
+    if i % 2 == 0:
+        continue
+    print("Item:{0}".format(i))
+
+#날짜
+years = list(range(2000,2023))
+print(years)
+days = list(range(1,32))
+print(days)
+
+#리스크 컴프리헨션(리스트 임베딩)
+lst = list(range(1,11))
+print([i**2 for i in lst if i > 5]) #6,7,8,9,10을 승수 
+
+#Tupled에 담은 데이터 
+tp = ("apple", "orange", "kiwi")
+print([len(i) for i in tp])
