119fc419437a9a3200c845d99bf7af914a676c3f
seungukk <suk638663@gamil.com>
2022-06-21 11:36:45 +0900

    4장 완료

--- DemoLoop.py
+++ DemoLoop.py
@@ -47,3 +47,23 @@ print([i**2 for i in lst if i > 5]) #6,7,8,9,10을 승수
 #Tupled에 담은 데이터 
 tp = ("apple", "orange", "kiwi")
 print([len(i) for i in tp])
+
+
+#필터링 하는 함수
+lst = [10,25, 30]
+iterL = filter(None, lst)
+for item in iterL:
+    print(item)
+
+print("---필터링---")
+def getBiggerThan20(i):
+    return i > 20
+
+iterL = filter(getBiggerThan20, lst)
+for item in iterL:
+    print(item)
+
+print("---람다함수---")
+iterL = filter(lambda i:i>20, lst)
+for item in iterL:
+    print(item)
