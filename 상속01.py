4fb4bfc22a4a96be7bcdb24009240d7175136e9f
seungukk <suk638663@gamil.com>
2022-06-21 15:44:50 +0900

    클래스 연습 완료

--- 상속01.py
+++ 상속01.py
@@ -1,22 +1,32 @@
+#부모 클래스 정의 
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
-        self.phoneNumber = phoneNumber
-
+        self.phoneNumber = phoneNumber #이름하고 전화번호만 받아서 초기화 
     def printInfo(self):
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
-
+    def methodA(self):
+        print("부모쪽 코드 실행")
+#자식 클래스 정의
 class Student(Person):
-    def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+    #상속받은 메서드를 덮어쓰기(재정의, override)
+    def __init__(self, name, phoneNumber, subject, studentID):  #데이터를 추가로 2개 더받아야해 덮어쓰기  
+        #명시적으로 부모 생성자 호출 
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+        #상속받은 메서드를 재정의
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+#object클래스에 정의된 스페셜 멤버 변수(__dict__ )
+# print(p.__dict__)
+# print(s.__dict__)
 
-
+p.printInfo()
+s.printInfo()  #person에 있는printInfo 실행됨 #컴공, 학번이안나옴 
+p.methodA()
+s.methodA()
